e4691b72f5d6776cd5f87e5679c434872fdf1e5f
bubbletiha <vaal6erite@gmail.com>
2025-11-12 00:01:03 +0500

    fiber

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,75 +1,28 @@
-// package main
-
-// import (
-// 	"io"
-// 	"log"
-// 	"net/http"
-// )
-
-// func healthCheck(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-// 	io.WriteString(w, "ok")
-// }
-
-// func main() {
-
-// 	mux := http.NewServeMux()
-
-// 	mux.HandleFunc("/healthz", healthCheck)
-
-// 	srv := &http.Server{
-// 		Addr:    ":8080",
-// 		Handler: mux,
-// 	}
-
-// 	log.Println("server started on :8080")
-// 	if err := srv.ListenAndServe(); err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"os"
-	"time"
 
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	// 1) грузим .env (если есть)
-	_ = godotenv.Load()
+	app := fiber.New(fiber.Config{
+		Prefork:       true,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Fiber",
+		AppName:       "Test App v1.0.1",
+	})
 
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		log.Fatal("DATABASE_URL is empty (проверь .env или переменные окружения)")
-	}
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendStatus(200)
+	})
 
-	// 2) создаём пул с таймаутом на подключение
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	app.Get("/live", func(c *fiber.Ctx) error {
+		return c.SendString("Server is living")
+	})
 
-	pool, err := pgxpool.New(ctx, dsn)
-	if err != nil {
-		log.Fatalf("pgxpool.New: %v", err)
-	}
-	defer pool.Close()
+	log.Fatal(app.Listen(":3000"))
 
-	// 3) проверяем, что коннект жив (Ping)
-	if err := pool.Ping(ctx); err != nil {
-		log.Fatalf("Ping failed: %v", err)
-	}
-
-	// 4) пробный запрос
-	var now time.Time
-	if err := pool.QueryRow(ctx, "SELECT NOW()").Scan(&now); err != nil {
-		log.Fatalf("QueryRow NOW() failed: %v", err)
-	}
-
-	fmt.Println("✅ Connected! Server time:", now.Format(time.RFC3339))
 }
